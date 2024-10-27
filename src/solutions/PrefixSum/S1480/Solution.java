b03e41ee4d972a2be8c2d1961f6f76f4ce8b88d5
Vlad Hvedchenya <hvdvlad@gmail.com>
2024-10-27 13:18:44 +0300

    added two solutions for S1480

--- src/solutions/PrefixSum/S1480/Solution.java
+++ src/solutions/PrefixSum/S1480/Solution.java
@@ -3,11 +3,28 @@ package solutions.PrefixSum.S1480;
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
 
         for (int i = 1; i < prefixSum.length; i++){
-            prefixSum[i] = nums[i - 1] + nums[i];
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
 
         return prefixSum;
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+class Solution2 {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++){
+            nums[i] = nums[i - 1] + nums[i];
+        }
+
+        return nums;
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
